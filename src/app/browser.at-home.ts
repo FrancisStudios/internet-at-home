96e936536f11be3b9416296ceae0a878ccd914d7
unknown <vlfmap@gmail.com>
2023-08-07 07:26:55 +0200

    added apps menu

--- src/app/browser.at-home.ts
+++ src/app/browser.at-home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 const __componentPrefix = './browser.at-home';
 
@@ -9,4 +9,20 @@ const __componentPrefix = './browser.at-home';
 })
 export class AppComponent {
   title = 'internet-at-home';
+  isAppsMenuEnabled: boolean = false;
+
+  /* TOGGLES APP LAUCNHER CONTEXT MENU */
+  toggleAppsMenu(){ 
+    if(!this.isAppsMenuEnabled){
+      this.isAppsMenuEnabled = true;
+    } else if(this.isAppsMenuEnabled){
+      this.isAppsMenuEnabled = false;
+    }
+  }
+
+  /* SHOULD CLOSE CONTEXT IF USER IS CLICKED AWAY*/
+  @HostListener('document:click', ['$event'])
+    documentClick(event: MouseEvent) {
+      if((event.target as HTMLImageElement).id !== "appsMenuToggler" && this.isAppsMenuEnabled === true) this.toggleAppsMenu();
+    }
 }
