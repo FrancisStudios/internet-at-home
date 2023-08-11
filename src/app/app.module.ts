c9dad337c43823b66802348a3b3172194b9404be
unknown <vlfmap@gmail.com>
2023-08-11 09:00:45 +0200

    created component for md-text-editor + md-article-viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { CrossroadComponent } from './crossroad/crossroad.component';
 import { DuegevHomeComponent } from '../duegev-wiki/duegev-home/duegev-home.component';
 import { DuegevAccountComponent } from 'src/duegev-wiki/duegev-account/duegev-account.component';
+import { MdTextEditorComponent } from '../duegev-wiki/_utils/md-text-editor/md-text-editor.component';
+import { MdArticleViewerComponent } from '../duegev-wiki/_utils/md-article-viewer/md-article-viewer.component';
 
 @NgModule({
   declarations: [
     BrowserAtHomeComponent,
     CrossroadComponent,
     DuegevHomeComponent,
-    DuegevAccountComponent
+    DuegevAccountComponent,
+    MdTextEditorComponent,
+    MdArticleViewerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/duegev-wiki/_utils/md-article-viewer/md-article-viewer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-md-article-viewer',
+  templateUrl: './md-article-viewer.component.html',
+  styleUrls: ['./md-article-viewer.component.css']
+})
+export class MdArticleViewerComponent {
+
+}
