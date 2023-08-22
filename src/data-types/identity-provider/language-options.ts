cb8e3a2fb56c3e1f8e6f0f5f9b3ca91c343ec813
unknown <vlfmap@gmail.com>
2023-08-22 20:26:52 +0200

    created UI for account/account

---
+++ src/data-types/identity-provider/language-options.ts
@@ -0,0 +1,4 @@
+export enum LanguageOptions {
+    DYNARI = 'Dynari',
+    ENGLISH = 'English'
+}
--- src/duegev-wiki/owners-menu-options/account-settings-viewer/account-settings-viewer.component.ts
+++ src/duegev-wiki/owners-menu-options/account-settings-viewer/account-settings-viewer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SessionStorageItems } from 'src/data-types/authentication/session-storage-items';
+import { UserData } from 'src/data-types/authentication/user-data';
+import { LanguageOptions } from 'src/data-types/identity-provider/language-options';
 
 @Component({
   selector: 'account-settings-viewer',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./account-settings-viewer.component.css']
 })
 export class AccountSettingsViewerComponent {
+  user: UserData = <UserData>JSON.parse(sessionStorage.getItem(SessionStorageItems.USER) || '');
+  languages: LanguageOptions[] = [LanguageOptions.ENGLISH, LanguageOptions.DYNARI];
 
+  getLanguageOptions(language_referrer: string): string {
+    switch(language_referrer){
+      case LanguageOptions.DYNARI:
+        return 'Dynari';
+      
+      case LanguageOptions.ENGLISH:
+        return 'English';
+
+      default:
+        return 'No preference';
+    }
+  } 
 }
