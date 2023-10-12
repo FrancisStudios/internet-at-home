2c583932a91e79a77a7ce0803b1329f03cb33b88
unknown <vlfmap@gmail.com>
2023-10-12 08:57:47 +0200

    added map editor ui

--- src/ultils/services/authentication-service/privileges-provider.service.ts
+++ src/ultils/services/authentication-service/privileges-provider.service.ts
@@ -14,7 +14,7 @@ export class DuegevPrivilegeProviderService {
         create endpoint where loggedInUser logs in and endpoint returns the 
         correct privileges -> this FN should return an Observable
         */
-        let response = new BehaviorSubject<string[]>(['sudo']);
+        let response = new BehaviorSubject<string[]>(['sudo', 'cartographer']);
         return response;
     }
 }
@@ -23,6 +23,7 @@ export const DuegevAccountPrivileges = [
     'sudo',         /* has access to everything */
     'recruiter',    /* has access to adding users */
     'default',      /* read, write, like, manage new year, social */
+    'cartographer', /* upload and edit maps */ 
 ]
 
 
